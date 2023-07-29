c6870cd4d897f60714a5f4235d71bd09ea5908e2
ii887522 <ii887522@gmail.com>
2023-07-29 13:30:22 +0800

    Change main py file to app.py

--- config.py
+++ config.py
@@ -2,6 +2,5 @@ customhost = "employee.cspnc5ouklrd.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee"
+custombucket = "yongchimin-bucket"
 customregion = "us-east-1"
-
