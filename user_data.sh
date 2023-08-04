42a09602dc0b35078354f22d258037bb5dc07349
ii887522 <ii887522@gmail.com>
2023-08-04 15:06:08 +0800

    Fix user data

--- user_data.sh
+++ user_data.sh
@@ -3,9 +3,8 @@
 
 #!/bin/bash
 dnf install git nodejs python-pip -y
-echo "github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl" > .ssh/known_hosts
-aws s3 cp s3://itp-infosys-key-bucket/id_ed25519 .ssh/id_ed25519
-chmod 400 .ssh/id_ed25519
+aws s3 cp s3://itp-infosys-key-bucket/id_ed25519 ~/.ssh/id_ed25519
+chmod 400 ~/.ssh/id_ed25519
 git clone git@github.com:ii887522/itp-infosys.git
 cd itp-infosys
 npm install
