e7ffdb4f7c28421e5dad68bbb73771af3bed754a
ii887522 <ii887522@gmail.com>
2023-08-04 13:22:46 +0800

    Fix could not access website

--- user_data.sh
+++ user_data.sh
@@ -2,11 +2,13 @@
 # Amazon Linux 2023
 
 #!/bin/bash
-dnf install git -y
-git clone https://github.com/ii887522/itp-infosys.git
+dnf install git nodejs python-pip -y
+echo "github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl" > .ssh/known_hosts
+aws s3 cp s3://itp-infosys-key-bucket/id_ed25519 .ssh/id_ed25519
+chmod 400 .ssh/id_ed25519
+git clone git@github.com:ii887522/itp-infosys.git
 cd itp-infosys
 npm install
 npx quasar build
-dnf install python-pip -y
 pip3 install -r requirements.txt
 python3 server/app.py
