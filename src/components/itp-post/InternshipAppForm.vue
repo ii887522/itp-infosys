86e9b80168ed1437188169d33afa1f7de17c5007
ii887522 <ii887522@gmail.com>
2023-08-08 00:30:13 +0800

    Add internship application form in internship details page

---
+++ src/components/itp-post/InternshipAppForm.vue
@@ -0,0 +1,50 @@
+<template>
+  <q-card>
+    <q-card-section>
+      <div class="text-h5 q-mb-lg">Internship Application</div>
+
+      <q-form @submit="apply">
+        <q-file
+          class="q-mb-lg"
+          name="resume_url"
+          max-files="1"
+          clearable
+          label-slot
+          v-model="resume"
+          outlined
+          counter
+          lazy-rules="ondemand"
+          :rules="[value => value || 'Resume is required']"
+        >
+          <template #label>
+            <q-icon name="attach_file" left size="xs" />
+            <span>Resume / CV</span>
+            <span class="text-negative"> *</span>
+          </template>
+        </q-file>
+
+        <q-input class="q-mb-lg" name="remarks" clearable label-slot autogrow v-model="remarks" outlined>
+          <template #label>
+            <q-icon name="description" left size="xs" />
+            <span>Note to Employer</span>
+          </template>
+        </q-input>
+
+        <div class="text-center">
+          <q-btn type="submit" icon="touch_app" label="Apply For This Internship Now" color="primary" />
+        </div>
+      </q-form>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const resume = ref()
+const remarks = ref('')
+
+function apply() {
+  console.log('APPLY FOR THIS INTERNSHIP NOW')
+}
+</script>
