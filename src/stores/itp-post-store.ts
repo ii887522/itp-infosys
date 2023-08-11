59cc3aebead5f46508c2e7c62d093cb84be76e32
ii887522 <ii887522@gmail.com>
2023-08-11 20:45:37 +0800

    Implement edit internship form

--- src/stores/itp-post-store.ts
+++ src/stores/itp-post-store.ts
@@ -1,6 +1,8 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import { type Internship, makeInternship } from 'src/models/itp-post'
+import { minAllowance, maxAllowance } from 'src/consts/itp-post'
+import { type Router } from 'vue-router'
 
 export const useInternshipSearchStore = defineStore('itp-post/internship-search', () => {
   const category = ref('')
@@ -22,4 +24,32 @@ export const useInternshipDetailsStore = defineStore('itp-post/internship-detail
   const value = ref<Internship>(makeInternship())
 
   return { value }
+})
+
+export const useInternshipEditStore = defineStore('itp-post/internship-edit', () => {
+  const title = ref('')
+  const categories = ref<string[]>([])
+  const allowanceRange = ref({ min: minAllowance, max: maxAllowance })
+  const location = ref('')
+  const vacancyCount = ref('')
+  const learningOutcomes = ref<string[]>([])
+  const description = ref('')
+
+  async function navigate(router: Router, internship: Internship) {
+    title.value = internship.title
+    categories.value = [...internship.categories]
+
+    allowanceRange.value = {
+      min: internship.min_allowance ?? minAllowance,
+      max: internship.max_allowance ?? maxAllowance,
+    }
+
+    location.value = internship.location
+    vacancyCount.value = String(internship.vacancy_count)
+    learningOutcomes.value = [...internship.learning_outcomes, '']
+    description.value = internship.description
+    await router.push('/emp/itp-post/internships/edit')
+  }
+
+  return { title, categories, allowanceRange, location, vacancyCount, learningOutcomes, description, navigate }
 })
